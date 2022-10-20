8e0338a0b99b4e0543e22d01ee8c279e00ed32c2
filip <filip_macek@hotmail.com>
2022-10-20 11:36:39 +0200

    solved leet_code/22_generate_parentheses

---
+++ leetCode/22_generate_parentheses.py
@@ -0,0 +1,45 @@
+"""
+---
+title: Generate parentheses
+number: 22
+difficulty: medium
+tags: ['String','Dynamic programming','Backtracking']
+url: https://leetcode.com/problems/generate-parentheses/
+solved: false
+---
+"""
+from typing import List
+
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+"""
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        return self.simple(n)
+    def simple(self,n:int):
+        # We could use the stack and count number of open and closed parenthesis
+        results = []
+        stack = []
+        def backtrack(openNum:int,closedNum:int):
+            if openNum == closedNum == n:
+                results.append("".join(stack))
+            if openNum < n:
+                stack.append("(")
+                backtrack(openNum+1,closedNum)
+                stack.pop()
+            if closedNum < openNum:
+                stack.append(")")
+                backtrack(openNum,closedNum+1)
+                stack.pop()
+
+
+
+        backtrack(0,0)
+        return results
+
+
+if __name__ == '__main__':
+    n = 2
+    print(Solution().generateParenthesis(n))
